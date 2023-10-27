3988bfeef6ece0a6ff7dae3e7a0e85b713312bcf
CsarChvz <79390377+CsarChvz@users.noreply.github.com>
2023-10-26 21:17:19 -0600

    a

--- src/infrastructure/repositories/ws.external.ts
+++ src/infrastructure/repositories/ws.external.ts
@@ -11,7 +11,10 @@ class WsTransporter extends Client implements LeadExternal {
   constructor() {
     super({
       authStrategy: new LocalAuth(),
-
+      puppeteer: {
+        headless: false,
+        args: ["--disable-setuid-sandbox", "--unhandled-rejections=strict"],
+      },
     });
 
     console.log("Iniciando....");
