47347f9aeddeab4eea26e2a7134353d392d67ea2
CsarChvz <79390377+CsarChvz@users.noreply.github.com>
2023-10-26 21:47:17 -0600

    asdf

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    {
+      name: "my-express-app",
+      script: "npm",
+      args: "run start",
+      cwd: "root/api-whatsapp-ts", // Reemplaza con la ruta a tu proyecto
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
